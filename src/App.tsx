659a96797364e445ecda81a78a282999c099ae7b
DevEmman <eolasunkanmi@gmail.com>
2024-12-15 21:35:36 +0000

    continuation on episode twwenty typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
+import { useCallback } from "react";
+import "./App.css";
 
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
-import './App.css'
+const Heading = ({title}: {title: string}) => <h2 className="text">{title}</h2>;
+
+const Box = ({children}: { children: React.ReactNode}) => (
+  <div style={{
+    padding: "1rem",
+    fontWeight: "bold",
+    }}> 
+    {children} 
+  </div>
+);
+
+const List: React.FunctionComponent<{items: string[]; onClick?: (item: string) => void}> = ({items, onClick }) => (
+  <ul>
+     {items.map((item, index) => (
+      <li key={index} onClick={() => onClick?.(item)}>{item}</li>
+     ) )}
+  </ul>
+)
+
+
 
 function App() {
-  return (
-    <div className=' '>
-      
+  const onListClick = useCallback((item: string) => {
+    alert(item)
+  }, []  );
+
+  return ( 
+    <div>
+        <Heading title="Introduction" />
+          <Box>hello typescript</Box>
+          <List items={["one","two","three"]} onClick={onListClick}/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
